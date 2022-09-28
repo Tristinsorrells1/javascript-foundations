8e44d5c1a05a90ca9757bb606fcb06b2c82a7de7
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-09-28 16:32:14 -0500

    5 passing

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+    constructor(name, rider, eat, hungry) {
+this.name = name;
+this.rider =rider;
+this.eat =eat;
+if (eat === undefined) {
+this.hungry = true;
+}
+else if (eat >= 3) {
+this.hungry = false;
+}
+}
 
+}
+
+
+
+
+
+
+ var smaug = new Dragon(); 
+
+module.exports = Dragon;
