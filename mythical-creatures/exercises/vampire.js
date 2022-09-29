29f3b7cfb13df8c13aa959c024c87d603dc4ec75
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-09-29 10:12:51 -0500

    6 passing vampire

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,8 +1,18 @@
 class Vampire {
-    constructor(name) {
+    constructor(name, pet, thirsty) {
     this.name =name;
+    this.pet =pet;
+    if (pet === undefined){
+        this.pet="bat"
+    }
+    this.thirsty =thirsty;
+    if (thirsty === undefined) {
+        this.thirsty=true
+    }
+ 
 }
 }
 
+var dracula = new Vampire();
 module.exports = Vampire;
 
