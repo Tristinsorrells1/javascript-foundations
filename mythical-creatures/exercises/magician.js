542680e38e20f39d8d00c061bee6e1b72bb7558e
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-10-20 08:05:01 -0500

    Pass 12 tests

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,45 @@
+
+class Magician {
+    constructor(magician, assistant, clothing, confidencePercentage) {
+        this.name = "The Great " + magician.name
+        this.assistant = magician.assistant
+        this.favoriteAccessory = magician.clothing || "top hat"
+        this.confidencePercentage = 10
+    }
+    performIncantation(spell) {
+       return spell.toUpperCase() + "!"
+    }
+    performTrick() {
+        if (this.favoriteAccessory === "top hat") {
+            this.confidencePercentage += 10
+            return 'PULL RABBIT FROM TOP HAT'
+        } 
+        else {
+            this.confidencePercentage += 10
+            return 'PULL DOVE FROM SLEEVE'
+        }
+    }
+
+    performShowStopper() {
+        if (this.confidencePercentage > 100 && this.assistant) {
+         return 'WOW! The magician totally just sawed that person in half!'
+        }
+        else {
+            return 'Oh no, this trick is not ready!'   
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Magician;
+
+
