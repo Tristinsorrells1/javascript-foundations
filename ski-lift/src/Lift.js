e196d5fe64167419cdc5ce1419bd8ad3e9c3ac43
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-10-24 16:26:44 -0500

    Practice pair

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,19 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
-}
+    constructor(liftLimit) {
+        this.inService = true
+        this.limit = liftLimit
+        this.skiers = []
+        this.safetyBar = 'up'
+    }
+
+    admitSkier(name, trueOrFalse) {
+        var people = new Skier(name)
+        this.skiers.push(people)
+        // console.log(this.skiers)
+    }
+
+} 
 
 module.exports = Lift;
