c99a51fd54f3233df2b9d146106906cbf907e895
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-10-24 15:45:22 -0500

    Pass 3

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,18 @@
 class Snowman {
-
+    constructor(snowmanDetails) {
+    this.carrots = snowmanDetails.carrots
+    this.coal = snowmanDetails.coal
+    this.buttons = snowmanDetails.buttons
+    this.snowballs = snowmanDetails.snowballs
+    this.magicHat = false
+    this.isWearingHat = false
+    }
+canWearMagicHat(){
+    this.magicHat = true
+    return true
+}
 }
 
+
 module.exports = Snowman;
+
