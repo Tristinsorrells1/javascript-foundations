c8186bdfce684dcd8f2a92a0882e76e99d0c7bfa
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-10-24 19:51:13 -0500

    Pass all snowman and human tests

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,34 @@
 var Snowman = require('./Snowman');
 
 class Human {
+    constructor(name){
+        this.name = name
+        this.wantsToBuildASnowman = true
+        this.materials = {
+            snowballs: 0,
+            coal: 0,
+            buttons: 0,
+            carrots: 0
+        }
+    }
 
+    gatherMaterials(material, num){
+        this.materials[material] += num;
+    }
+
+    buildASnowman(){
+        this.snowman = new Snowman(this.materials)
+        return this.snowman
+    }
+
+    placeMagicHat(snowman){
+        if(this.snowman.magicHat === false){
+            return 'I guess I didn\'t build it correctly.'
+        }
+        else {
+        return this.snowman.canWearMagicHat()
+        }
+    }
 }
 
 module.exports = Human;
--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -4,17 +4,17 @@ class Snowman {
     this.coal = snowmanDetails.coal
     this.buttons = snowmanDetails.buttons
     this.snowballs = snowmanDetails.snowballs
-    this.magicHat = false
+    this.magicHat = true
     this.isWearingHat = false
     }
 canWearMagicHat(){
     if (this.buttons < 5 || this.coal < 2 || this.carrots < 1 || this.snowballs < 2) {
         this.magicHat = false
-        return false
+        return 'I guess I didn\'t build it correctly.'
     }
     else {
         this.magicHat = true
-        return true
+        return 'Woah, this snowman is coming to life!'
     }
 }
 }
