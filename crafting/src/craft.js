64a6c09f3e9eb19bfcc017e6a93e7b95141c7b2f
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-02 08:49:47 -0500

    Pass all test

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,28 @@
-class Craft {
 
+class Craft {
+    constructor(craftInfo) {
+        this.name = craftInfo.type
+        this.materials = craftInfo.materials
+        this.completed= false
+
+    }
+    completeCraft(){
+        this.completed = true
+        return 'All done! It looks great!'
+    }
+
+
+    calculateProjectTotal() {
+        console.log(this.materials)
+        var totalCost = 0
+        for (var i = 0; i < this.materials.length; i++) {
+            totalCost += this.materials[i].price * this.materials[i].amount
+        }
+        return totalCost
+    }
 }
 
+
+
 module.exports = Craft;
+
--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,25 @@
 class Material {
+    constructor (name, price, amount, units) {
+        this.name = name
+        this.price = price
+        this.amount = amount
+        this.units = units
 
+    }
+    useMaterial(num){
+        if (this.amount > num) {
+        return `You now have ${this.amount -= num} ${this.units} of ${this.name} left.`
+        }
+        else {
+            return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+        }
+    }
+    calculateMaterialCost(){
+        return this.price *= this.amount
+    }
 }
 
 module.exports = Material;
+
+//  npm test crafting/test/material-test.js
+//  npm test crafting/test/craft-test.js
