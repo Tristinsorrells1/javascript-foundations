2ea0390a68a01be1917387d440d946aa8053bc9c
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-04 10:45:57 -0500

    Pass all tests

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -8,16 +8,35 @@ class VendingMachine {
 
     }
 
-    addSnacks(snack){
-        for (var i; i < this.snacks.length; i++) {
-            if (!this.snacks.includes(this[i].name)) {
-                this.snacks.push(this.name)
-             }
-            else {
-             return 'Sorry, that snack is already stocked! Try adding a different snack.'}
+    addSnacks(snackInfo){
+       var newSnack = new Snack(snackInfo)
+       for (var i = 0; i < this.snacks.length; i++) {
+           if (this.snacks[i].name === newSnack.name.name) {
+               return 'Sorry, that snack is already stocked! Try adding a different snack.'
+            }
+        } 
+    this.snacks.push(newSnack.name)
     }
-    
-}
+
+    purchaseSnack(snackName, money){
+        for (var i = 0; i < this.snacks.length; i++) {
+            if (this.snacks[i].name === snackName) {
+                if (this.snacks[i].itemsInStock && money > this.snacks[i].price ){
+                    this.snacks[i].itemsInStock --
+                    return `Success! Here is $${money - this.snacks[i].price} back!`
+                }
+                else if (money < this.snacks[i].price) {
+                    return 'Sorry, not enough payment. Please add more money.'
+                }
+                else return `Sorry, no items in stock. Try another item.`
+            }
+        }  
+
+    }
+
 }
 
+
 module.exports = VendingMachine;
+
+// npm test vending-machine/test/vending-machine-test.js
