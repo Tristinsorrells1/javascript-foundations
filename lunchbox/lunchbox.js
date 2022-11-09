cf044decc08827efb7071b8d595aff3d96b25cc4
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-08 18:30:08 -0600

    Pass all tests

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,27 @@
-class LunchBox {
+var Snack = require('./snack');
 
+class LunchBox {
+    constructor(lunchbox) {
+        this.owner = lunchbox.owner
+        this.material = lunchbox.madeOf
+        this.shape = lunchbox.shape
+        this.color = lunchbox.color
+        this.snacks = []
+    }
+    acquireSnack(newSnack) {
+        var snacky = newSnack
+        newSnack.isInLunchBox = true
+        this.snacks.push(newSnack)
+    }
+    findHealthySnacks(){
+        var healthySnacks = []
+        for (var i = 0; i < this.snacks.length; i++) {
+           if (this.snacks[i].checkForHealthy()) {
+                healthySnacks.push(this.snacks[i].type)
+            }
+        }
+        return healthySnacks
+    }
 }
 
 module.exports = LunchBox;
