7709978b9a0acfa8ef4b1751d7380dafc0aa5624
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-08 23:14:08 -0600

    Pass all tests

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,28 @@
+var Golfer = require('./golfer.js')
+
 class GolfCourse {
+    constructor(name, difficulty, openiningNum, features){
+        this.name = name
+        this.difficulty = difficulty
+        this.openings = openiningNum
+        this.features = features
+        this.currentlyPlaying = []
+
+    }
+    checkInGroup(group){
+        for (var i = 0; i < group.length; i++){
+            if (group.length <= this.openings) {
+            this.currentlyPlaying.unshift(group[i].name)
+            }
+            else {
+                return 'Sorry, we are currently booked! Please come back later.'
+            }
+        }
+         this.openings -= group.length
+        return 'You\'re checked in. Have fun!'
+    }
+    }
 
 
-}
 
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,53 @@
 class Golfer {
-
-
+constructor(golferInfo){
+    this.name = golferInfo.name
+    this.handicap = golferInfo.handicap
+    this.frustration = 0
+    this.confidence = 0
+}
+    calculateAvg(par){
+        return `I usually shoot a ${this.handicap + par} on average.`
+    }
+    
+    playRound(golfInfo) {
+    if (golfInfo.difficulty === "hard"){
+        this.frustration += 500
+    }
+    else if (golfInfo.difficulty === "moderate")
+        this.frustration += 100
+    }
+    hitTheRange(){
+        this.confidence += 10
+    }
+    marvel(course){
+        return `I love the ${course.features[0]} and ${course.features[1]} on this course!`
+    }
+    whatYaShoot(num){
+        if (num === 0){
+            this.frustration = 10
+            return `Booyah!`
+        }
+        else if (num === 4){
+            this.frustration += 20
+            return 'Doh!'
+        }
+        else if (num === 2) {
+            this.frustration = 30
+            return 'Doh!'
+        }
+        else if (num === -2){
+            this.frustration = 0
+            return 'I AM THE GREATEST GOLFER ALIVE!'
+        }
+        else if (num === 3){
+            this.frustration += 20
+            return 'Doh!'
+        }
+        else if (num === -1){
+            this.frustration = 0
+            return 'I AM THE GREATEST GOLFER ALIVE!'
+        }
+    }
 }
 
 module.exports = Golfer;
